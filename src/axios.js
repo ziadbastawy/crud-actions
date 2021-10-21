aba391e5f16dafd40a6d0a9171a99ef0bef152e3
Ziad bastawy <ziadmahmoud413@gmail.com>
2021-10-21 12:34:50 +0200

    add axios inceptors

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,30 @@
 import axios from "axios";
+import router from "./router/index";
 
-export default axios.create({
+const apiClient = axios.create({
   baseURL: "https://taskfrontendapi.azurewebsites.net/api/",
-  headers: {
-    Authorization: "Bearer " + localStorage.getItem("token") || "",
-  },
 });
+
+apiClient.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("token") || "";
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+apiClient.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  if (error.response.status == 401) {
+    localStorage.removeItem("token");
+     router.push({
+        name: "login"
+      });
+  }
+  return Promise.reject(error);
+});
+export default  apiClient;
